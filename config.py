958852c45c694b4e1f21b35dcc1aa9bb6058f6c0
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-04-28 22:39:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,18 +4,18 @@
 import os
 from os import getenv
 # ---------------R---------------------------------
-API_ID = int(os.environ.get("API_ID"))
+API_ID = int(os.environ.get("26590556"))
 # ------------------------------------------------
-API_HASH = os.environ.get("API_HASH")
+API_HASH = os.environ.get("ce6abb8f2b7d2c82471d38e9f50c2a52")
 # ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("6832304682:AAGPoAccqeYNsM62_Gmg8EZSOOFR7RW0gyA")
 # -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME")
+BOT_USERNAME = os.environ.get("@EMXES_BOT")
 # ------------------X------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID"))
+OWNER_ID = int(os.environ.get("1813320767"))
 # ------------------X------------------------------
 # ------------------------------------------------
-LOGGER_ID = int(os.environ.get("LOGGER_ID"))
+LOGGER_ID = int(os.environ.get("-1002078575375"))
 # ------------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL")
+MONGO_URL = os.environ.get("mongodb+srv://cluster0.80xb4d0.mongodb.net/" --apiVersion 1 --username dracogameing1")
 # ------------------------------------------------
